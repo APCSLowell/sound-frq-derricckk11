f1ac1a438eaa01c2eb0062615eabcb1754f8a7b3
derricckk11 <132044722+derricckk11@users.noreply.github.com>
2025-04-10 23:56:08 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -23,7 +23,7 @@ public class Sound
         samples[i] = limit; 
         changed++;
         
-      } else if(samples[i] < limit){
+      } else if(samples[i] < -limit){
 
         samples[i] = -limit; 
         changed++;
